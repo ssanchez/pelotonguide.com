8e2d74d81371e3e65ae906ffed8f97af7dd44684
Spencer Sanchez <dev@spencersanchez.com>
2018-06-30 10:39:53 -0400

    Add linting

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,8 +7,8 @@ import Header from './header'
 import './layout.css'
 
 const Layout = ({ children, data }) => (
-  <StaticQuery
-    query={graphql`
+	<StaticQuery
+		query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -17,33 +17,33 @@ const Layout = ({ children, data }) => (
         }
       }
     `}
-    render={data => (
+		render={(data) => (
       <>
-        <Helmet
-          title={data.site.siteMetadata.title}
-          meta={[
-            { name: 'description', content: 'Sample' },
-            { name: 'keywords', content: 'sample, something' },
-          ]}
-        />
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
-          {children}
-        </div>
+	<Helmet
+		meta={[
+        		{ name: 'description', content: 'Sample' },
+        		{ name: 'keywords', content: 'sample, something' },
+        	]}
+		title={data.site.siteMetadata.title}
+	/>
+	<Header siteTitle={data.site.siteMetadata.title} />
+	<div
+		style={{
+        		margin: '0 auto',
+        		maxWidth: 960,
+        		padding: '0px 1.0875rem 1.45rem',
+        		paddingTop: 0,
+        	}}
+	>
+		{children}
+	</div>
       </>
-    )}
-  />
+		)}
+	/>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
