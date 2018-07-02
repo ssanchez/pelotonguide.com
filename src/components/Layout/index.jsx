a17b55351858e16bf8cc5e4c5ac20b29f08e62e3
Spencer Sanchez <dev@spencersanchez.com>
2018-07-02 11:49:32 -0400

    Make Layout body component

--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -2,17 +2,10 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
-import styled from 'react-emotion'
 
 import Header from './Header'
+import Body from './Body'
 import './layout.css'
-
-const LayoutWrapper = styled.div`
-	margin: 0 auto;
-	max-width: 960;
-	padding: 0 1.0875rem 1.45rem;
-	padding-top: 0;
-`
 
 const Layout = ({ children }) => (
 	<StaticQuery
@@ -35,9 +28,9 @@ const Layout = ({ children }) => (
 					title={data.site.siteMetadata.title}
 				/>
 				<Header siteTitle={data.site.siteMetadata.title} />
-				<LayoutWrapper>
+				<Body>
 					{children}
-				</LayoutWrapper>
+				</Body>
 			</>
 		)}
 	/>
