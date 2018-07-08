3baca798742ce73f2523e4e6c6907e4980f87414
Spencer Sanchez <dev@spencersanchez.com>
2018-07-07 21:14:17 -0400

    Move page-level graphql queries out of StaticQuery

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,43 @@
 import React from 'react'
+import { object } from 'prop-types'
 import {
 	graphql,
 	Link,
-	StaticQuery,
 } from 'gatsby'
 import Layout from 'components/Layout'
 
-const IndexPage = () => (
-	<StaticQuery
-		query={graphql`
-			query IndexPageQuery {
-				allContentfulPost(limit: 1000) {
-					edges {
-						node {
-							slug
-						}
-					}
+const IndexPage = ({ data }) => {
+	const posts = data.allContentfulPost.edges
+	return (
+		<>
+			<Layout>
+				<Link to='theme-ride-guide'>Theme ride guide</Link>
+				{posts.map(({ node }) => (
+					<div>
+						<Link to={node.slug}>
+							{node.slug}
+						</Link>
+					</div>
+				))}
+			</Layout>
+		</>
+	)
+}
+
+IndexPage.propTypes = {
+	data: object,
+}
+
+export const indexPageQuery = graphql`
+	query IndexPageQuery {
+		allContentfulPost(limit: 1000) {
+			edges {
+				node {
+					slug
 				}
 			}
-		`}
-		render={(data) => {
-			const posts = data.allContentfulPost.edges
-			return (
-				<>
-					<Layout>
-						<Link to='theme-ride-guide'>Theme ride guide</Link>
-						{posts.map(({ node }) => (
-							<div>
-								<Link to={node.slug}>
-									{node.slug}
-								</Link>
-							</div>
-						))}
-					</Layout>
-				</>
-			)
-		}}
-	/>
-)
+		}
+	}
+`
 
 export default IndexPage
--- src/pages/theme-ride-guide.jsx
+++ src/pages/theme-ride-guide.jsx
@@ -1,45 +1,41 @@
 import React from 'react'
-import {
-	graphql,
-	// Link,
-	StaticQuery,
-} from 'gatsby'
+import { object } from 'prop-types'
+import { graphql } from 'gatsby'
 import Layout from 'components/Layout'
 
-const RidesPage = () => (
-	<StaticQuery
-		query={graphql`
-			query RidesPageQuery {
-				allContentfulRide(limit: 1000) {
-					edges {
-						node {
-							coach {
-								name
-							}
-							date
-							isVisible
-							minutes
-							more
-							name
-							type
-						}
+const RidesPage = ({ data }) => {
+	const rides = data.allContentfulRide.edges
+	return (
+		<Layout>
+			{rides.map(({ node }) => (
+				<div>{node.name} {node.coach.name}</div>
+			))}
+		</Layout>
+	)
+}
+
+RidesPage.propTypes = {
+	data: object,
+}
+
+export const ridesPageQuery = graphql`
+	query RidesPageQuery {
+		allContentfulRide(limit: 1000) {
+			edges {
+				node {
+					coach {
+						name
 					}
+					date
+					isVisible
+					minutes
+					more
+					name
+					type
 				}
 			}
-		`}
-		render={(data) => {
-			const rides = data.allContentfulRide.edges
-			return (
-				<>
-					<Layout>
-						{rides.map(({ node }) => (
-							<div>{node.name} {node.coach.name}</div>
-						))}
-					</Layout>
-				</>
-			)
-		}}
-	/>
-)
+		}
+	}
+`
 
 export default RidesPage
